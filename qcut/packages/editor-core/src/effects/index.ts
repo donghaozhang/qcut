78dc294102400e59e525a0360e8129cbaaf420ba
Donghao Zhang <zdhpeter@gmail.com>
2026-07-18 23:42:28 +1000

    feat(effects): export keyframe helpers

--- qcut/packages/editor-core/src/effects/index.ts
+++ qcut/packages/editor-core/src/effects/index.ts
@@ -5,3 +5,14 @@ export {
 	validateEffectRenderProgram,
 	type EffectRenderProgramValidation,
 } from "./render-program.js";
+export {
+	EFFECT_KEYFRAME_PARAMETER_KEYS,
+	findEffectKeyframeAtTime,
+	isEffectKeyframeParameter,
+	removeEffectKeyframe,
+	resolveEffectAnimationValue,
+	resolveEffectParametersAtTime,
+	trimEffectAnimations,
+	upsertEffectKeyframe,
+	type EffectKeyframeParameter,
+} from "./keyframes.js";
