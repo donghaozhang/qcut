3bd2ed74d25a4d9dd3431804ac2e1c1636cc1e91
Donghao Zhang <zdhpeter@gmail.com>
2026-08-04 23:44:37 +1000

    feat(import): type writeback preload API

---
+++ qcut/electron/preload-types/api-types/jianying-same-profile-writeback-api.ts
@@ -0,0 +1,5 @@
+import type { JianyingSameProfileWritebackAPI } from "../../jianying-same-profile-writeback-contract";
+
+export interface JianyingSameProfileWritebackPreloadAPI {
+	jianyingSameProfileWriteback: JianyingSameProfileWritebackAPI;
+}
