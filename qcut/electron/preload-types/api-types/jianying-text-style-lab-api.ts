ee9437685da62bf2c566ca077f612218283023b4
Donghao Zhang <zdhpeter@gmail.com>
2026-08-11 21:10:18 +1000

    feat(preload): type Jianying text style lab API

---
+++ qcut/electron/preload-types/api-types/jianying-text-style-lab-api.ts
@@ -0,0 +1,5 @@
+import type { JianyingTextStyleLabAPI } from "../../jianying-text-style-lab-contract";
+
+export interface JianyingTextStyleLabPreloadAPI {
+	jianyingTextStyleLab?: JianyingTextStyleLabAPI;
+}
