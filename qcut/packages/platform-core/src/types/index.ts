608e97132f338e3dcf66100b6bde83402c7e3bd5
Donghao Zhang <zdhpeter@gmail.com>
2026-08-23 15:08:08 +1000

    feat(platform): wire index

--- qcut/packages/platform-core/src/types/index.ts
+++ qcut/packages/platform-core/src/types/index.ts
@@ -8,5 +8,6 @@ export * from "./base.js";
 export * from "./core-api.js";
 export * from "./media-api.js";
 export * from "./integration-api.js";
+export * from "./sticker-lab-api.js";
 export * from "./claude-api.js";
 export * from "./platform.js";
