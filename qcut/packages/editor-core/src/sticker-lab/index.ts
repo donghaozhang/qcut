b813a019d9c64eb2fd6b92347b6cc33f5aaad9a1
Donghao Zhang <zdhpeter@gmail.com>
2026-08-26 04:36:24 +1000

    feat(sticker-lab): export deterministic runtime APIs

--- qcut/packages/editor-core/src/sticker-lab/index.ts
+++ qcut/packages/editor-core/src/sticker-lab/index.ts
@@ -8,3 +8,39 @@ export {
 	type PrivateStickerCatalogDefinition,
 	type PrivateStickerCatalogId,
 } from "./private-catalogs";
+export { createAlphaVideoRuntimeDescriptor } from "./runtime-alpha-video.js";
+export { parseAtlasRuntimeDescriptor } from "./runtime-atlas.js";
+export { evaluateStickerRuntime } from "./runtime-evaluate.js";
+export { parseDirectGifRuntimeDescriptor } from "./runtime-gif.js";
+export { createPngSequenceRuntimeDescriptor } from "./runtime-sequence.js";
+export { assertStickerRuntimeDescriptor } from "./runtime-validation.js";
+export type {
+	AlphaVideoLayout,
+	AlphaVideoMaskChannel,
+	AlphaVideoMaskSettings,
+	AlphaVideoProgressKeyframe,
+	AlphaVideoRuntimeDescriptor,
+	AlphaVideoRuntimeState,
+	AtlasRuntimeDescriptor,
+	AtlasRuntimeFrame,
+	AtlasRuntimeState,
+	DirectGifRuntimeDescriptor,
+	DirectGifRuntimeFrame,
+	DirectGifRuntimeState,
+	PngSequenceRuntimeDescriptor,
+	PngSequenceRuntimeFrame,
+	PngSequenceRuntimeState,
+	StickerRuntimeActiveState,
+	StickerRuntimeCompletion,
+	StickerRuntimeDescriptor,
+	StickerRuntimeInactiveReason,
+	StickerRuntimeInactiveState,
+	StickerRuntimeNormalizedRect,
+	StickerRuntimePixelRect,
+	StickerRuntimePixelSize,
+	StickerRuntimeRepeat,
+	StickerRuntimeState,
+	StickerRuntimeTimelineWindow,
+} from "./runtime-model.js";
+export { StickerRuntimeError } from "./runtime-model.js";
+export type { PngSequenceFrameInput } from "./runtime-sequence.js";
