136ab86eb3ee4fd54c10b07833b31e9d4c66939b
Donghao Zhang <zdhpeter@gmail.com>
2026-08-28 15:47:19 +1000

    feat(cutout): update alpha mask fusion

--- qcut/electron/jianying-person-cutout/native/alpha-mask-fusion.hpp
+++ qcut/electron/jianying-person-cutout/native/alpha-mask-fusion.hpp
@@ -14,4 +14,7 @@ std::vector<std::uint8_t> fusePersonAlpha(
     const std::vector<std::uint8_t> &visionAlpha,
     PersonAlphaFusionConfig config = {});
 
+void composeSourceAlphaInPlace(const std::vector<std::uint8_t> &rgba,
+                               std::vector<std::uint8_t> &matteAlpha);
+
 } // namespace qcut::matting
