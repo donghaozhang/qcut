89296e4717d7fbcd0942f0ddb429b4a3069ad22d
Donghao Zhang <zdhpeter@gmail.com>
2026-09-05 23:19:13 +1000

    test(dialog): verify cover overlay stacking overrides

--- qcut/apps/web/src/components/ui/dialog.test.tsx
+++ qcut/apps/web/src/components/ui/dialog.test.tsx
@@ -13,6 +13,23 @@ import {
 } from "@/components/ui/dialog";
 
 describe("Dialog Component", () => {
+	it("allows a cover modal to raise its overlay without changing default dialogs", () => {
+		render(
+			<Dialog open>
+				<DialogContent
+					overlayClassName="cover-overlay-test z-[1000]"
+					className="z-[1001]"
+					aria-describedby={undefined}
+				>
+					<DialogTitle>Cover layer</DialogTitle>
+				</DialogContent>
+			</Dialog>
+		);
+		expect(document.querySelector(".cover-overlay-test")).toHaveClass(
+			"z-[1000]"
+		);
+		expect(screen.getByRole("dialog")).toHaveClass("z-[1001]");
+	});
 	it("renders dialog trigger", () => {
 		const { getByText } = render(
 			<Dialog>
